7729e505a7b01c30f78f8f62a01a603cb7d4cb22
Debjani Paul <debjanideb@hotmail.com>
2020-03-18 14:17:17 -0700

    Soled Pig Latin

--- pigLatin.js
+++ pigLatin.js
@@ -1,16 +1,13 @@
-/* Perform a search and replace on the sentence using the arguments provided and return the new
-sentence.
+/* Translate the provided string to pig latin.
 
-First argument is the sentence to perform the search and replace on.
+Pig Latin takes the first consonant (or consonant cluster) of an English word, moves it to the
+end of the word and suffixes an "ay".
 
-Second argument is the word that you will be replacing (before).
+If a word begins with a vowel you just add "way" to the end.
 
-Third argument is what you will be replacing the second argument with (after).
+If a word does not contain a vowel, just add "ay" to the end.
 
-Note
-Preserve the case of the first character in the original word when you are replacing it.
-For example if you mean to replace the word "Book" with the word "dog", 
-it should be replaced as "Dog"*/
+Input strings are guaranteed to be English words in all lowercase.*/
 
 
 
